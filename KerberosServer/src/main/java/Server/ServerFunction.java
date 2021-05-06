ff7fc11ac06b9609a937056a7cf89c23802d9c3d
yilinduan <472379433@qq.com>
2021-05-06 21:11:01 +0800

    创建Server文件夹； 创建FunctionSet类。

---
+++ KerberosServer/src/main/java/Server/ServerFunction.java
@@ -0,0 +1,14 @@
+package Server;
+
+import Kerberos.FunctionSet;
+
+public class ServerFunction extends FunctionSet {
+    //继承方法集
+    ServerFunction(){
+        //调用函数，发送本地证书给AS
+    }
+    @Override
+    public boolean Integer(String message){
+        return true;
+    }
+}
