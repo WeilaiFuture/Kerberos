599c5fa08389d452e5c41930070708d524e28355
WeilaiFuture <43932541+WeilaiFuture@users.noreply.github.com>
2021-05-07 10:39:40 +0800

    Update json

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Client
         private void Hyperlink_Click(object sender, RoutedEventArgs e)
         {
             Hyperlink link = sender as Hyperlink;
-            Process.Start(new ProcessStartInfo(link.NavigateUri.AbsoluteUri));
+            Process.Start(new ProcessStartInfo("https://www.baidu.com/s?wd=wpf%20%E8%83%8C%E6%99%AF%E5%8A%A8%E5%9B%BE&pn=10&oq=wpf%20%E8%83%8C%E6%99%AF%E5%8A%A8%E5%9B%BE&tn=18029102_2_dg&ie=utf-8&usm=1&fenlei=256&rsv_idx=1&rsv_pq=a4f0792d0092be12&rsv_t=b08cE%2F2uEk1KDuLu9VX9x%2B7lKsdG5JmSd0RZvGA8qiNL5ibU6tEvE1fKuykVOAQ2nYQr%2Bw&rsv_page=1"));
         }
     }
 }
