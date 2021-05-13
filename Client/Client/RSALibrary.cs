b039fdb5bdac8417b0b5d647bac2c2e4f1cd1c45
WeilaiFuture <43932541+WeilaiFuture@users.noreply.github.com>
2021-05-13 11:27:01 +0800

    Update 设计文档第二稿、RSA

---
+++ Client/Client/RSALibrary.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class RSALibrary
+    {
+        public string PrivateKey{ get; set; }
+        public string PublicKey { get; set; }
+
+        #region RSA 加密解密
+        #region RSA 的密钥产生
+        /// <summary>
+        /// RSA产生密钥
+        /// </summary>
+        /// <param name="xmlKeys">私钥</param>
+        /// <param name="xmlPublicKey">公钥</param>
+        public void RSAKey(out string xmlKeys, out string xmlPublicKey)
+        {
+            try
+            {
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                PrivateKey=xmlKeys = rsa.ToXmlString(true);
+                PublicKey=xmlPublicKey = rsa.ToXmlString(false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region RSA加密函数
+        //############################################################################## 
+        //RSA 方式加密 
+        //KEY必须是XML的形式,返回的是字符串 
+        //该加密方式有长度限制的！
+        //############################################################################## 
+
+        /// <summary>
+        /// RSA的加密函数
+        /// </summary>
+        /// <param name="xmlPublicKey">公钥</param>
+        /// <param name="encryptString">待加密的字符串</param>
+        /// <returns></returns>
+        public string RSAEncrypt(string xmlPublicKey, string encryptString)
+        {
+            try
+            {
+                byte[] PlainTextBArray;
+                byte[] CypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPublicKey);
+                PlainTextBArray = (new UnicodeEncoding()).GetBytes(encryptString);
+                CypherTextBArray = rsa.Encrypt(PlainTextBArray, false);
+                Result = Convert.ToBase64String(CypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }     
+        #endregion
+
+        #region RSA的解密函数        
+        /// <summary>
+        /// RSA的解密函数
+        /// </summary>
+        /// <param name="xmlPrivateKey">私钥</param>
+        /// <param name="decryptString">待解密的字符串</param>
+        /// <returns></returns>
+        public string RSADecrypt(string xmlPrivateKey, string decryptString)
+        {
+            try
+            {
+                byte[] PlainTextBArray;
+                byte[] DypherTextBArray;
+                string Result;
+                System.Security.Cryptography.RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(xmlPrivateKey);
+                PlainTextBArray = Convert.FromBase64String(decryptString);
+                DypherTextBArray = rsa.Decrypt(PlainTextBArray, false);
+                Result = (new UnicodeEncoding()).GetString(DypherTextBArray);
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+        #endregion
+
+        #region RSA数字签名
+        #region 获取Hash描述表        
+        /// <summary>
+        /// 获取Hash描述表
+        /// </summary>
+        /// <param name="strSource">待签名的字符串</param>
+        /// <param name="strHashData">Hash描述</param>
+        /// <returns></returns>
+        public bool GetHash(string strSource, ref string strHashData)
+        {
+            try
+            {
+                //从字符串中取得Hash描述 
+                byte[] Buffer;
+                byte[] HashData;
+                System.Security.Cryptography.HashAlgorithm MD5 = System.Security.Cryptography.HashAlgorithm.Create("MD5");
+                Buffer = System.Text.Encoding.GetEncoding("GB2312").GetBytes(strSource);
+                HashData = MD5.ComputeHash(Buffer);
+                strHashData = Convert.ToBase64String(HashData);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region RSA签名
+        /// <summary>
+        /// RSA签名
+        /// </summary>
+        /// <param name="strKeyPrivate">私钥</param>
+        /// <param name="strHashbyteSignature">待签名Hash描述</param>
+        /// <param name="strEncryptedSignatureData">签名后的结果</param>
+        /// <returns></returns>
+        public bool SignatureFormatter(string strKeyPrivate, string strHashbyteSignature, ref string strEncryptedSignatureData)
+        {
+            try
+            {
+                byte[] HashbyteSignature;
+                byte[] EncryptedSignatureData;
+                HashbyteSignature = Convert.FromBase64String(strHashbyteSignature);
+                System.Security.Cryptography.RSACryptoServiceProvider RSA = new System.Security.Cryptography.RSACryptoServiceProvider();
+                RSA.FromXmlString(strKeyPrivate);
+                System.Security.Cryptography.RSAPKCS1SignatureFormatter RSAFormatter = new System.Security.Cryptography.RSAPKCS1SignatureFormatter
+
+    (RSA);
+                //设置签名的算法为MD5 
+                RSAFormatter.SetHashAlgorithm("MD5");
+                //执行签名 
+                EncryptedSignatureData = RSAFormatter.CreateSignature(HashbyteSignature);
+                strEncryptedSignatureData = Convert.ToBase64String(EncryptedSignatureData);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region RSA 签名验证
+        /// <summary>
+        /// RSA签名验证
+        /// </summary>
+        /// <param name="strKeyPublic">公钥</param>
+        /// <param name="strHashbyteDeformatter">Hash描述</param>
+        /// <param name="strDeformatterData">签名后的结果</param>
+        /// <returns></returns>
+        public bool SignatureDeformatter(string strKeyPublic, string strHashbyteDeformatter, string strDeformatterData)
+        {
+            try
+            {
+                byte[] DeformatterData;
+                byte[] HashbyteDeformatter;
+                HashbyteDeformatter = Convert.FromBase64String(strHashbyteDeformatter);
+                System.Security.Cryptography.RSACryptoServiceProvider RSA = new System.Security.Cryptography.RSACryptoServiceProvider();
+                RSA.FromXmlString(strKeyPublic);
+                System.Security.Cryptography.RSAPKCS1SignatureDeformatter RSADeformatter = new
+
+    System.Security.Cryptography.RSAPKCS1SignatureDeformatter(RSA);
+                //指定解密的时候HASH算法为MD5 
+                RSADeformatter.SetHashAlgorithm("MD5");
+                DeformatterData = Convert.FromBase64String(strDeformatterData);
+                if (RSADeformatter.VerifySignature(HashbyteDeformatter, DeformatterData))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    #endregion
+    #endregion 
+    }
+}
