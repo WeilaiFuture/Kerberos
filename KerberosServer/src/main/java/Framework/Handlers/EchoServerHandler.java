6d0994e8c675a484b10605cc57dd0b74d13ea9e2
chang <ayaglch@163.com>
2021-05-23 23:07:15 +0800

    完善了famework，添加了session和sessionhandler，现在可以开始写代码了。

---
+++ KerberosServer/src/main/java/Framework/Handlers/EchoServerHandler.java
@@ -0,0 +1,35 @@
+package Framework.Handlers;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //do something msg
+        ByteBuf buf = (ByteBuf)msg;
+        byte[] data = new byte[buf.readableBytes()];
+        buf.readBytes(data);
+        String request = new String(data, "utf-8");
+        System.out.println("Server: " + request);
+        //写给客户端
+        String response = "我是反馈的信息";
+        ctx.writeAndFlush(Unpooled.copiedBuffer("888".getBytes()));
+        ctx.write(Unpooled.copiedBuffer("888".getBytes()));
+        ctx.flush();
+
+        //.addListener(ChannelFutureListener.CLOSE);
+
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
