b8b8a77287482ac62b18dc9affbf0eb131466371
yilinduan <472379433@qq.com>
2021-05-24 21:52:04 +0800

    添加报文处理逻辑。 添加部分数据库处理函数。

--- KerberosServer/src/main/java/Server/ServerDataBase.java
+++ KerberosServer/src/main/java/Server/ServerDataBase.java
@@ -2,23 +2,56 @@ package Server;
 
 import Json.MyStruct;
 
+import java.sql.*;
 import java.util.LinkedList;
 
- public class ServerDataBase {
+public class ServerDataBase {
     /*
     包含所有数据库操作的封装函数；
      */
-    static public boolean connectData() {
+    static private Connection con=null; // 声明Connection对象
+    static public Connection connectData() {
         /*
         连接数据库；
          */
-        return false;
+        Connection conn = null;
+        // no ssl certificate:
+        String url = "jdbc:mysql://" +
+                "rm-uf6t4cbyfz681x569jo.mysql.rds.aliyuncs.com:3306/kerbors" +
+                "?sslmode=require" +
+                "&connectTimeout=3000" +
+                "&socketTimeout=60000";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, "w123", "Wl123456");
+            System.out.println("Database connected");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("连接数据库错误");
+            return null;
+        }
+        con=conn;
+        return conn;
     }
 
     static public boolean wCertif(MyStruct struct) {
         /*
         向数据库存入证书；
          */
+        try {
+            String sql="insert into 'Certificate' ('version' ,'serial' ,'deadline' ,'name' ,'pk') values(\""
+                    +struct.certificate.getVersion()+"\",\""
+                    +struct.certificate.getSerial()+"\",\""
+                    +struct.certificate.getDeadline()+"\",\""
+                    +struct.certificate.getName()+"\",\""
+                    +struct.certificate.getPk()+ "\")";
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("存储证书错误");
+        }
         return false;
     }
 
@@ -26,32 +59,89 @@ import java.util.LinkedList;
         /*
         向数据库存入注册信息；
          */
+        try {
+            Statement sql=con.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("存储注册信息错误");
+        }
         return false;
     }
     static public boolean wLogin(String ID,String status){
         /*
         向数据库写入登陆状态；
          */
+        try {
+            String sql="update 'users' set ('status') values(1)";
+            Statement statement=con.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("写入登陆状态错误");
+        }
         return false;
     }
-    static public LinkedList rFriendList(String ID){
+    static public LinkedList<MyStruct.Friend> rFriendList(String ID){
         /*
         查询好友列表；
          */
-        return new LinkedList();
+        LinkedList<MyStruct.Friend> friends=new LinkedList<MyStruct.Friend>();
+        try {
+            String sql="SELECT * FROM 'friend' WHERE 'me'=\"" + ID + "\"";
+            Statement statement=con.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                MyStruct.Friend friend=new MyStruct.Friend();
+                friend.setRemark(result.getString("remark"));
+                friend.setStartTime(result.getInt("startTime"));
+
+                String sql1="SELECT 'tname' FROM 'tags' WHERE 'tid'="+result.getInt("tid")+  " and 'uid'=\""+ID+"\"";
+                Statement statement1=con.createStatement();
+                ResultSet result1 = statement.executeQuery(sql1);
+                friend.setTid(result1.getString("tname"));
+
+                MyStruct.User user=rSearchID(result.getString("ta"));
+                friend.setU(user);
+
+                friends.addLast(friend);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("查询好友列表错误");
+        }
+        return friends;
     }
-    static public boolean rSearchID(String ID){
+    static public LinkedList<String> rOnLineFriend(String ID){
+        /*
+        查询在线状态好友ID；
+         */
+        return new LinkedList<String>();
+    }
+     static public LinkedList<String> rGroupUser(String ID){
+        /*
+        查询群内成员ID；
+         */
+         return new LinkedList<String>();
+     }
+    static public MyStruct.User rSearchID(String ID){
         /*
         查询用户；
          */
-        return false;
+        return new MyStruct.User();
     }
     static public boolean rInfo(String ID){
         /*
         查询个人信息；
+        与查询用户功能重复，暂时保留
          */
-        return false;
-    }
+         return false;
+     }
+     static public boolean wInfo(MyStruct.User user){
+        /*
+        修改个人信息；
+         */
+         return false;
+     }
     static public boolean wAddF(String IDA,String IDB){
         /*
         修改好友列表，添加好友；
--- KerberosServer/src/main/java/Server/ServerHandler.java
+++ KerberosServer/src/main/java/Server/ServerHandler.java
@@ -2,6 +2,10 @@ package Server;
 
 import Json.MyJson;
 import Json.MyStruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
 
 import static Server.ServerDataBase.*;
 
@@ -9,6 +13,8 @@ public class ServerHandler {
     /*
         包含所有收到的报文
     */
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
     public boolean Certif(String message){
         /*
         head=0001;
@@ -38,6 +44,7 @@ public class ServerHandler {
         //缺少一个数据库函数
         return false;
     }
+    //注册功能属于web server
     public boolean regeister(String message){
         /*
         head=1001;
@@ -65,6 +72,9 @@ public class ServerHandler {
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
         //存入数据库
         wRegister(mystruct);
+        //发送ACK
+        order.setStatusReport(true);
+        //发送
         return false;
     }
     public boolean searchFriendList(String message){
@@ -78,6 +88,9 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //查询数据库
+        LinkedList<MyStruct.Friend> friends=rFriendList(order.getSrc());
+        //发送
         return false;
     }
     public boolean hello(String message){
@@ -91,12 +104,16 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //查询数据库，所有上线好友ID
+        LinkedList<String>onLineFriends=rOnLineFriend(order.getSrc());
+        //发送
         return false;
     }
     public boolean heart(String message){
         /*
         head=1006;
         心跳信息；
+        什么都不用做；
          */
         //获取报文
         MyJson.Order order =MyJson.StringToOrder(message);
@@ -114,6 +131,9 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //查询数据库，用户信息
+        MyStruct.User user=rSearchID(mystruct.user.getUid());
+        //发送
         return false;
     }
     public boolean logout(String message){
@@ -127,6 +147,9 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //查询数据库，所有上线好友ID
+        LinkedList<String>onLineFriends=rOnLineFriend(order.getSrc());
+        //发送
         return false;
     }
     public boolean information(String message){
@@ -138,6 +161,9 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //查询数据库，用户信息
+        MyStruct.User user=rSearchID(mystruct.user.getUid());
+        //发送
         return false;
     }
     public boolean changeInfo(String message){
@@ -150,6 +176,8 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        //更改数据库，个人信息
+        wInfo(mystruct.user);
         return false;
     }
     public boolean privateChat(String message){
@@ -161,6 +189,17 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        /*
+        101 文本消息
+        102 图片消息
+        103 语音消息
+        104 表情消息
+        105 动图消息
+        9001 添加好友信息
+        9006 同意加好友
+        9007 拒绝加好友
+         */
+        //转发
         return false;
     }
     public boolean publicChat(String message){
@@ -172,6 +211,17 @@ public class ServerHandler {
         MyJson.Order order =MyJson.StringToOrder(message);
         //获取消息
         MyStruct mystruct=MyJson.StringToStruct(order.getExtend());
+        /*
+        需要区分具体消息类型；
+        9002 加入群聊信息
+        9003 组建群聊信息
+        9004 退出群聊信息
+        9008同意加群
+        9009拒绝加群
+         */
+        //查询群内好友
+        LinkedList<String> GroupUsers=rGroupUser(order.getDst());
+        //转发
         return false;
     }
 
